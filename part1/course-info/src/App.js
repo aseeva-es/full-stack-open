c5e0f4ba587e0ce0ce834567b894bc1d58f1e271
Elena Aseeva <aseeva.e.s@gmail.com>
2023-02-26 13:33:28 +0200

    task 1.5 is done

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -29,34 +29,36 @@ const Content = ({ parts }) => {
 const Total = ({ parts }) => {
   return (
     <>
-      <p> Number of exercises
-         { parts[0].exercises + parts[1].exercises + parts[2].exercises }</p>
+      <p> Number of exercises 
+         {' ' + (parts[0].exercises + parts[1].exercises + parts[2].exercises) }</p>
     </>
   );
 };
 
 const App = () => {
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-  const course = "Half Stack application development";
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header title={course} />
-      <Content parts = {parts}></Content>
+      <Header title={course.name} />
+      <Content parts = {course.parts}></Content>
 
-      <Total parts = {parts} />
+      <Total parts = {course.parts} />
     </div>
   );
 };
