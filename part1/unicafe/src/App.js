eb686576795da95ce778e693ec62bb6cd8c7d25f
Elena Aseeva <aseeva.e.s@gmail.com>
2023-02-27 16:39:05 +0200

    1.9: unicafe step4 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,6 +3,29 @@ import { useState } from 'react'
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
 const Statistics = (props) => <p> {props.title} {props.value}</p>
+
+const AllStats = ({ good, neutral, bad, total }) => {
+  if( total === 0 ){
+    return (
+      <p>No feedback given</p>
+      )
+    } 
+    const average = () => ((good - bad) / total); 
+    const positive = () => (good * 100 / total ) + '%';
+  return (
+    <>
+    <Statistics title = 'Good' value = { good } />
+    <Statistics title = 'Neutral' value = { neutral } />
+    <Statistics title = 'Bad' value = { bad } />
+    <Statistics title = 'Total' value = { total }/>
+    <Statistics title = 'Average' value = { average() } />
+    <Statistics title = 'Positive' value = { positive() } s/>
+    </>
+  )
+  
+}
+
+
   
   
 
@@ -20,7 +43,6 @@ const App = () => {
     setTotal( upgradeGood + neutral + bad);
   }
   const handleNeutralClick = () => {
-    console.log('handleClick')
     const upgradeNeutral = neutral + 1;
     setNeutral(upgradeNeutral  );
     setTotal(upgradeNeutral + good +bad );
@@ -38,12 +60,8 @@ const App = () => {
         <Button onClick = {handleNeutralClick} text = "Neutral"/>
         <Button onClick = {handleBadClick} text = "Bad"/>
         <h3>Statistics</h3>
-        <Statistics title = 'Good' value = { good }/>
-        <Statistics title = 'Neutral' value = { neutral }/>
-        <Statistics title = 'Bad' value = { bad }/>
-        <Statistics title = 'Total' value = { total }/>
-        <Statistics title = 'Average' value = {(good - bad) / total }/>
-        <Statistics title = 'Positive' value = {good * 100 / total }/>
+        <AllStats good = { good } neutral = {neutral} bad = { bad } total = {total}/>
+       
     </div>
   )
 }
